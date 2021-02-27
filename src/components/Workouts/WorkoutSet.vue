fc68a50eebaa4cf0c725cf4a56bb1d2e901832a1
nickmly <nickmly@gmail.com>
2021-02-27 08:35:37 -0500

    Refactor

--- src/components/Workouts/WorkoutSet.vue
+++ src/components/Workouts/WorkoutSet.vue
@@ -2,12 +2,12 @@
   <form @submit.prevent="$emit('addSet')">
     <input
       type="number"
-      v-on:input="$emit('addWeight', $event.target.value)"
+      @input="$emit('addWeight', $event.target.value)"
       placeholder="Weight"
     />
     <input 
       type="number" 
-      v-on:input="$emit('addReps', $event.target.value)" 
+      @input="$emit('addReps', $event.target.value)" 
       placeholder="Reps" 
     />
     <button type="submit">Done</button>
