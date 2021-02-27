ffcb7f40e0f9e9cac6bd8d3298c833aefbf22568
nickmly <nickmly@gmail.com>
2021-02-26 19:23:06 -0500

    Get and post workouts + sets from API

--- src/components/Workouts/Log.vue
+++ src/components/Workouts/Log.vue
@@ -3,18 +3,18 @@
     <h2>Workout log</h2>
     <ul v-if="!addingExercise">
       <button @click="showExerciseList">Add</button>
-      <li v-for="exercise in log" :key="exercise.id">
+      <li v-for="exercise in workouts" :key="exercise.id">
         <div class="exercise">
           <div>
             <h4>{{ exercise.title }}</h4>
-            <button @click="showSetForm(exercise.id)">+</button>
+            <button @click="showSetForm(exercise.workout_id)">+</button>
           </div>
-          <ul v-if="addingSet !== exercise.id" class="set-list">
+          <ul v-if="addingSet !== exercise.workout_id" class="set-list">
             <li v-for="(set, index) in exercise.sets" :key="index">
               {{ set.weight }}lbs / {{ set.reps }} reps
             </li>
           </ul>
-          <form v-else @submit.prevent="addSet(exercise.id)">
+          <form v-else @submit.prevent="addSet(exercise.workout_id)">
             <input
               type="number"
               v-model.number="addingSetWeight"
@@ -48,7 +48,8 @@ export default {
   data() {
     return {
       exercises: this.$store.state.exercises,
-      log: [],
+      log: {},
+      workouts: [],
       addingExercise: false,
       addingSet: null,
       addingSetWeight: null,
@@ -56,28 +57,57 @@ export default {
       currentId: 0,
     };
   },
-  mounted() {
-    HTTP.get("/log", {
+  async mounted() {
+    // Get the log for today
+    const logResponse = await HTTP.get("/log", {
       params: {
         uid: this.$store.state.user.uid,
         date: new Date().toLocaleDateString(),
       },
-    }).then((response) => {
-      this.log = response.data;
+    });
+    this.log = logResponse.data.length === 0 ? null : logResponse.data[0];
+    // If we have no log today, exit; No need to get the workouts from a non-existing log
+    if (logResponse.data.length === 0) return;
+    // Get the workouts from this log
+    const workoutsResponse = await HTTP.get("/workout", {
+      params: {
+        log_id: this.log.id,
+      },
+    });
+    // For each workout, get the exercise + all the sets associated and add to the workouts array
+    workoutsResponse.data.forEach((w) => {
+      const exercise = this.exercises.find(
+        (e) => e.id === Number(w.exercise_id)
+      );
+      HTTP.get("/sets", {
+        params: {
+          workout_id: w.id,
+        },
+      }).then((response) => {
+        this.workouts.push({
+          id: w.exercise_id,
+          title: exercise.title,
+          workout_id: w.id,
+          sets: response.data,
+        });
+      });
     });
   },
   methods: {
     showExerciseList() {
-      if (this.log.length === 0) {
+      if (this.log === null) {
         // if there is no log for today then add one
-        HTTP
-          .post("/log", {
-            uid: this.$store.state.user.uid,
+        HTTP.post("/log", {
+          uid: this.$store.state.user.uid,
+          date: new Date().toLocaleDateString(),
+        }).then((response) => {
+          this.log = {
+            id: response.data,
+            user_id: this.$store.state.user.uid,
             date: new Date().toLocaleDateString(),
-          })
-          .then(() => {
-            this.addingExercise = true;
-          });
+          };
+          this.addingExercise = true;
+        });
       } else {
         this.addingExercise = true;
       }
@@ -86,22 +116,36 @@ export default {
       this.addingSet = id;
     },
     addExercise(id) {
-      const exercise = this.exercises.find((e) => e.id === id);
-      this.log.push({
-        id: this.currentId,
-        title: exercise.title,
-        sets: [],
+      const exercise = this.$store.state.exercises.find((e) => e.id === id);
+      // Add a workout to the DB with the exercise id
+      HTTP.post("/workout", {
+        log_id: this.log.id,
+        exercise_id: id,
+      }).then((response) => {
+        this.workouts.push({
+          workout_id: response.data,
+          id: exercise.id,
+          title: exercise.title,
+          sets: [],
+        });
+        this.addingExercise = false;
       });
-      this.currentId++;
-      this.addingExercise = false;
     },
     addSet(id) {
-      const exercise = this.log.find((e) => e.id === id);
-      exercise.sets.push({
+      // Add a set to the DB with the workout id
+      HTTP.post("/set", {
+        workout_id: id,
         weight: this.addingSetWeight,
         reps: this.addingSetReps,
+      }).then((response) => {
+        const workout = this.workouts.find((w) => w.workout_id === id);
+        workout.sets.push({
+          id: response.data,
+          weight: this.addingSetWeight,
+          reps: this.addingSetReps,
+        });
+        this.addingSet = false;
       });
-      this.addingSet = false;
     },
   },
 };
