614327a6e2aa1635080df7ce9790d03c796b55f4
nickmly <nickmly@gmail.com>
2021-02-27 08:01:07 -0500

    Date picker added

--- src/components/Workouts/Log.vue
+++ src/components/Workouts/Log.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h2>Workout log</h2>
+    <app-date-picker
+      v-model="currentDate"
+      wrapper-class="date-picker"
+      format="yyyy-MM-dd"
+    />
     <ul v-if="!addingExercise">
       <button @click="showExerciseList">Add</button>
       <li v-for="exercise in workouts" :key="exercise.id">
@@ -42,9 +47,13 @@
   </div>
 </template>
 <script>
+import Datepicker from "vuejs-datepicker";
 import { HTTP } from "../../axios";
 
 export default {
+  components: {
+    appDatePicker: Datepicker,
+  },
   data() {
     return {
       exercises: this.$store.state.exercises,
@@ -54,57 +63,79 @@ export default {
       addingSet: null,
       addingSetWeight: null,
       addingSetReps: null,
-      currentId: 0,
+      currentDate: new Date(),
     };
   },
   async mounted() {
-    // Get the log for today
-    const logResponse = await HTTP.get("/log", {
-      params: {
-        uid: this.$store.state.user.uid,
-        date: new Date().toLocaleDateString(),
-      },
-    });
-    this.log = logResponse.data.length === 0 ? null : logResponse.data[0];
-    // If we have no log today, exit; No need to get the workouts from a non-existing log
-    if (logResponse.data.length === 0) return;
-    // Get the workouts from this log
-    const workoutsResponse = await HTTP.get("/workout", {
-      params: {
-        log_id: this.log.id,
-      },
-    });
-    // For each workout, get the exercise + all the sets associated and add to the workouts array
-    workoutsResponse.data.forEach((w) => {
-      const exercise = this.exercises.find(
-        (e) => e.id === Number(w.exercise_id)
-      );
-      HTTP.get("/sets", {
-        params: {
-          workout_id: w.id,
-        },
-      }).then((response) => {
-        this.workouts.push({
-          id: w.exercise_id,
-          title: exercise.title,
-          workout_id: w.id,
-          sets: response.data,
-        });
-      });
-    });
+    await this.populateLog();
+  },
+  computed: {
+    formattedDate() {
+      return this.currentDate.toLocaleDateString();
+    },
+  },
+  watch: {
+    currentDate: function() {
+      this.resetLog();
+      this.populateLog();
+    }
   },
   methods: {
+    resetLog() {
+      this.log = {};
+      this.workouts = [];
+      this.addingExercise = false;
+      this.addingSet = null;
+      this.addingSetWeight = null;
+      this.addingSetReps = null;
+    },
+    async populateLog() {
+      // Get the log for today
+      const logResponse = await HTTP.get("/log", {
+        params: {
+          uid: this.$store.state.user.uid,
+          date: this.formattedDate,
+        },
+      });
+      this.log = logResponse.data.length === 0 ? null : logResponse.data[0];
+      // If we have no log today, exit; No need to get the workouts from a non-existing log
+      if (logResponse.data.length === 0) return;
+      // Get the workouts from this log
+      const workoutsResponse = await HTTP.get("/workout", {
+        params: {
+          log_id: this.log.id,
+        },
+      });
+      // For each workout, get the exercise + all the sets associated and add to the workouts array
+      workoutsResponse.data.forEach((w) => {
+        const exercise = this.exercises.find(
+          (e) => e.id === Number(w.exercise_id)
+        );
+        HTTP.get("/sets", {
+          params: {
+            workout_id: w.id,
+          },
+        }).then((response) => {
+          this.workouts.push({
+            id: w.exercise_id,
+            title: exercise.title,
+            workout_id: w.id,
+            sets: response.data,
+          });
+        });
+      });
+    },
     showExerciseList() {
       if (this.log === null) {
         // if there is no log for today then add one
         HTTP.post("/log", {
           uid: this.$store.state.user.uid,
-          date: new Date().toLocaleDateString(),
+          date: this.formattedDate,
         }).then((response) => {
           this.log = {
             id: response.data,
             user_id: this.$store.state.user.uid,
-            date: new Date().toLocaleDateString(),
+            date: this.formattedDate,
           };
           this.addingExercise = true;
         });
@@ -165,5 +196,9 @@ ul {
 }
 .set-list {
   display: inline-block;
+}
+.date-picker {
+  display: flex;
+  justify-content: center;
 }
 </style>
