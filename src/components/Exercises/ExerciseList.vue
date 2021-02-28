f496437375c26caafd6c75313b3eff948ae7b863
nickmly <nickmly@gmail.com>
2021-02-28 08:52:51 -0500

    Filter by search term or muscle group

--- src/components/Exercises/ExerciseList.vue
+++ src/components/Exercises/ExerciseList.vue
@@ -1,18 +1,65 @@
 <template>
-  <div>
-    <button
-      v-for="exercise in exercises"
-      :key="exercise.id"
-      @click="$emit('addExercise', exercise.id)"
-    >
-      {{ exercise.title }}
-    </button>
+  <div class="exercise-list">
+    <input v-if="!currentMuscle" type="text" placeholder="Search" v-model="searchExercise" />
+    <div v-if="!currentMuscle && !searchExercise" class="exercise-list__list">
+      <button
+        v-for="muscle in muscles"
+        :key="muscle"
+        @click="currentMuscle = muscle"
+      >
+        {{ muscle }}
+      </button>
+    </div>
+    <div v-else class="exercise-list__list">
+      <button class="back-btn" v-if="currentMuscle" @click="goBack">Go Back</button>
+      <button
+        v-for="exercise in exercisesFiltered"
+        :key="exercise.id"
+        @click="$emit('addExercise', exercise.id)"
+      >
+        {{ exercise.title }}
+      </button>
+    </div>
   </div>
 </template>
 <script>
 export default {
   props: {
-    exercises: Array
-  }
-}
+    exercises: Array,
+  },
+  data() {
+    return {
+      muscles: [],
+      currentMuscle: null,
+      searchExercise: "",
+    };
+  },
+  mounted() {
+    this.muscles = new Set(this.exercises.map((e) => e.muscles));
+  },
+  methods: {
+    goBack() {
+      this.currentMuscle = null;
+      this.searchMuscle = "";
+    },
+  },
+  computed: {
+    exercisesFiltered() {
+      return this.currentMuscle ? this.exercises.filter((e) => e.muscles === this.currentMuscle) : this.exercises.filter((e) => e.title.toLowerCase().includes(this.searchExercise.toLowerCase()));
+    },
+  },
+};
 </script>
+<style scoped>
+.exercise-list {
+  display: flex;
+  flex-direction: column;
+}
+.exercise-list__list {
+  display: flex;
+  flex-direction: column;
+}
+.back-btn {
+  align-self: flex-start;
+}
+</style>
